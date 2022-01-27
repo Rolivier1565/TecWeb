04716bcce6c1befbde8f27917cff83d9ef8fff42
EdoardoGasparini <edoardo.gasparini.2@studenti.unipd.it>
2022-01-27 17:29:29 +0100

    bottoni funzionanti e in bound

--- forum.php
+++ forum.php
@@ -55,6 +55,15 @@
   }else{
     $listaPost="<p>Ci scusiamo ma i sistemi non sono al momento disponibili riprova più tardi.</p>";
   }
-    $listaPost.='<form method="post" action="forum.php"><input type="hidden" name="ultimopost" value="'. $lastpost .'"><input type="submit" id="prev" name="prev" value="prev"/><input type="submit" id="next" name="next" value="next"/>';
+  if(end($_SESSION['maxidm'])==$_SESSION['max']){
+      $listaPost.='<form method="post" action="forum.php"><input type="hidden" name="ultimopost" value="'. $lastpost .'"><input type="submit" id="next" name="next" value="next"/>';
+  }
+  else{
+      if($lastpost==1){
+        $listaPost.='<form method="post" action="forum.php"><input type="hidden" name="ultimopost" value="'. $lastpost .'"><input type="submit" id="prev" name="prev" value="prev"/>';
+      }else{
+        $listaPost.='<form method="post" action="forum.php"><input type="hidden" name="ultimopost" value="'. $lastpost .'"><input type="submit" id="prev" name="prev" value="prev"/><input type="submit" id="next" name="next" value="next"/>';
+    }
+  }
     echo str_replace("<listaPost/>",$listaPost, $paginaHTML);
 ?>
